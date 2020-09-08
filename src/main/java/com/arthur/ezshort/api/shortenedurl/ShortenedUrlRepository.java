304c3782b5ef7efaa2f227df9d9711798038fea0
Arthur <arthur.cdfreitas@gmail.com>
2020-09-07 21:13:37 -0300

    refactor: removing unused queries

--- src/main/java/com/arthur/ezshort/api/shortenedurl/ShortenedUrlRepository.java
+++ src/main/java/com/arthur/ezshort/api/shortenedurl/ShortenedUrlRepository.java
@@ -1,18 +1,8 @@
 package com.arthur.ezshort.api.shortenedurl;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShortenedUrlRepository extends JpaRepository<ShortenedUrl, Long> {
-
-	@Query(value = "SELECT * FROM LINK WHERE sh_url=?1", nativeQuery = true)
-	Optional<ShortenedUrl> findByShortUrl(String shortUrlParam);
-
-	@Query(value = "SELECT * FROM LINK WHERE or_url=?1", nativeQuery = true)
-	Optional<ShortenedUrl> findByOriginalUrl(String url);
-
 }
