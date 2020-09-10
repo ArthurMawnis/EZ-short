c18dfc9b0417aad02cae90257f86f85b4027d729
Arthur <arthur.cdfreitas@gmail.com>
2020-09-09 22:08:44 -0300

    adding custom exception for invalid url inputs

---
+++ src/main/java/com/arthur/ezshort/api/exceptions/InvalidUrlException.java
@@ -0,0 +1,11 @@
+package com.arthur.ezshort.api.exceptions;
+
+public class InvalidUrlException extends Exception {
+
+    private static final long serialVersionUID = 5013744369305426563L;
+
+    public InvalidUrlException(String message) {
+	super(message);
+    }
+
+}
