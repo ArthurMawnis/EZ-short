b354c487a1b9ab22d07b37bded5c10ac753b7d03
Arthur <arthur.cdfreitas@gmail.com>
2020-09-09 22:14:26 -0300

    refactor: changing InvalidUrlException
     from Exception to RunTimeException

--- src/main/java/com/arthur/ezshort/api/exceptions/InvalidUrlException.java
+++ src/main/java/com/arthur/ezshort/api/exceptions/InvalidUrlException.java
@@ -1,6 +1,6 @@
 package com.arthur.ezshort.api.exceptions;
 
-public class InvalidUrlException extends Exception {
+public class InvalidUrlException extends RuntimeException {
 
     private static final long serialVersionUID = 5013744369305426563L;
 
